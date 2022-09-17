ece8337baca8f53ccbb6d8977879d6d4b5c44da8
jeroxz12 <jeronimolago@hotmail.es>
2022-09-16 22:49:02 -0300

    "2 commit"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { StudentsListComponent } from './components/students-list/students-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentsListComponent
   ],
   imports: [
     BrowserModule
